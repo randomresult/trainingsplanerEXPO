7d23e65cc1ec1e746b46be9aada15e546ca3e62c
AgentService <markusromaniw@gmx.de>
2026-04-21 17:20:53 +0200

    feat(ui): add Text primitive with iOS HIG variants

--- components/ui/index.ts
+++ components/ui/index.ts
@@ -1,2 +1,5 @@
 export { Screen } from './Screen';
 export type { ScreenProps } from './Screen';
+
+export { Text } from './Text';
+export type { TextProps, TextVariant, TextColor, TextWeight } from './Text';
