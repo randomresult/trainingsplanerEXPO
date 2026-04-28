85a97f0cd9e5bf45e3b519733e10506511f1fc6b
AgentService <markusromaniw@gmx.de>
2026-04-28 14:37:51 +0200

    feat(routes): add library-pick-draft route for no-trainingId flow

---
+++ app/library-pick-draft.tsx
@@ -0,0 +1,5 @@
+import { LibraryDraftPickerContainer } from '@/components/screens/LibraryDraftPickerContainer';
+
+export default function LibraryPickDraftScreen() {
+  return <LibraryDraftPickerContainer />;
+}
